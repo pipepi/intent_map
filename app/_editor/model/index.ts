41d6281df8a4bda25b63a0e0f1c88005cabc408d
bit <bit23665@gmail.com>
2026-08-09 19:12:04 +0800

    docs: add editor model notes

---
+++ app/_editor/model/index.ts
@@ -0,0 +1,13 @@
+// ecs: component
+// node:
+//  traits:[]
+//      position_identity: {x_time:number,z_object_identity:number,y_r_object_type:number,
+//                          l_local_x:number,l_local_y:number,l_local_z:number,
+//                          w_world_x:number,w_world_y:number,w_world_z:number,
+//                          i_identity:number}
+//      tags:{t_type:{asix:string="x_time,y_r_object_type,z_object_identity,e_event"},
+//            theme:string="dark,light"
+//            name:string="",title:string="",description:string="",keywords:string="",}
+//      relations:{o_objects:[],n_node_parent:position_identity,n_node_child:[position_identity],
+//                             p_port_inputs:[],q_port_outputs:[]}
+//      implementation: React.Component
